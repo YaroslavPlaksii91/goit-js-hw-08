83cc212b796f8a72c293b3ce550739e30c79ad16
YaroslavPlaksii91 <yarosyk@ukr.net>
2022-01-27 08:18:28 +0200

    some changes

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,11 +4,11 @@ const form = document.querySelector('.feedback-form');
 const LOCALSTORAGE_KEY = 'feedback-form-state';
 const feedbackData = {};
 
-onReloadPage();
-form.addEventListener('input', throttle(saveFeedback, 500));
+onPageReload();
+form.addEventListener('input', throttle(saveFeedbackToStorage, 500));
 form.addEventListener('submit', onFormSubmit);
 
-function saveFeedback(event) {
+function saveFeedbackToStorage(event) {
   feedbackData[event.target.name] = event.target.value;
   localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(feedbackData));
 }
@@ -16,19 +16,22 @@ function saveFeedback(event) {
 function onFormSubmit(event) {
   event.preventDefault();
 
-  const submitedFeedback = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
-  if (submitedFeedback) {
-    console.log(submitedFeedback);
-  }
+  feedbackData.email = form.elements.email.value;
+  feedbackData.message = form.elements.message.value;
+  console.log(feedbackData);
 
   event.currentTarget.reset();
   localStorage.removeItem(LOCALSTORAGE_KEY);
 }
 
-function onReloadPage() {
-  const feedback = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
-  if (feedback) {
-    form.elements.email.value = feedback.email;
-    form.elements.message.value = feedback.message;
+function onPageReload() {
+  try {
+    const feedback = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+    if (feedback) {
+      form.elements.email.value = feedback.email;
+      form.elements.message.value = feedback.message;
+    }
+  } catch (error) {
+    console.error(error.message);
   }
 }
